70625206d04f7c75d6780745cae170e3d47d22e5
unknown <jesusvaz@gmail.com>
2020-09-24 21:48:53 -0600

    Updates

---
+++ precache-manifest.1c5a03aa0e273398e6e0ab7904f75824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f9ffe23d69965277a981cef951b211",
+    "url": "/restaurante/index.html"
+  },
+  {
+    "revision": "362f4968b92560d7f3ba",
+    "url": "/restaurante/static/css/main.f8d27fea.chunk.css"
+  },
+  {
+    "revision": "41c3ffdb2f528d283c43",
+    "url": "/restaurante/static/js/2.f33a777f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/restaurante/static/js/2.f33a777f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362f4968b92560d7f3ba",
+    "url": "/restaurante/static/js/main.49939c4c.chunk.js"
+  },
+  {
+    "revision": "988e240f61665a4ac388",
+    "url": "/restaurante/static/js/runtime-main.4dd1ae7f.js"
+  }
+]);
